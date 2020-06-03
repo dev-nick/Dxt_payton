4b469e6ae6da9b1826a1aae08c25dcb8e5231d0a
DhineshCool <dhineshcool585@gmail.com>
2020-06-03 11:57:34 +0530

    Update scripts

--- gettool.sh
+++ gettool.sh
@@ -11,12 +11,8 @@ sudo apt install bc bash git-core gnupg build-essential \
     zip curl make automake autogen autoconf autotools-dev libtool shtool python \
     m4 gcc libtool zlib1g-dev flex bison libssl-dev
 
-# Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r35 aarch64-linux-android-4.9
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r35 arm-linux-androideabi-4.9
-
 # Clone AnyKernel3
 git clone https://github.com/DhineshCool/AnyKernel3 anykernel
 
-#Download Clang
-git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b10.0 cl11
+#Download Clang and ToolChain
+git clone https://github.com/kdrag0n/proton-clang.git clang
