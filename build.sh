1cd9df6d963a279e51d1e8aa71d3f2cff8265cfe
DhineshCool <dhineshcool585@gmail.com>
2020-07-02 10:36:03 +0530

    DxT v2.0

--- build.sh
+++ build.sh
@@ -38,8 +38,8 @@ cd ..
 cd $ZIP_DIR
 cp $KERN_IMG zImage
 make normal &>/dev/null
-rm -rf Dxt-v1.9.zip
-zip -r9 Dxt-v1.9.zip * -x .git README.md *placeholder
+rm -rf DxT-v2.0.zip
+zip -r9 DxT-v2.0.zip * -x .git README.md *placeholder
 echo "Flashable zip generated under $ZIP_DIR."
 cd ..
 # Build end
